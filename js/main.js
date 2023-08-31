aef563f32da57273d0c2f05840d6ed0aa2730091
luigitarallo <tarallo.gigi@gmail.com>
2023-08-31 21:34:18 +0200

    feat: add addNewToDo function

--- js/main.js
+++ js/main.js
@@ -8,11 +8,16 @@ createApp({
         { text: "Dar da mangiare al pesce rosso", done: false },
         { text: "Fare la spesa", done: true },
       ],
+      newToDo: { text: "", done: false },
     };
   },
   methods: {
     deleteToDo(index) {
       this.toDo.splice(index, 1);
+    },
+    addNewToDo() {
+      const newToDoCopy = { ...this.newToDo };
+      this.toDo.push(newToDoCopy);
     },
   },
 }).mount("#app");
