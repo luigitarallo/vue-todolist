eac3a4effc8e8a67fa1310c332f7b1dfa3668a3d
luigitarallo <tarallo.gigi@gmail.com>
2023-08-31 15:39:37 +0200

    feat: add object to vue data

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      toDo: [
+        { text: "Pagare le bollette", done: true },
+        { text: "Dar da mangiare al pesce sorro", done: false },
+        { text: "Fare la spesa", done: "true" },
+      ],
+    };
   },
 }).mount("#app");
