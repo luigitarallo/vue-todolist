ddfd573a7074047e36ef9ec1685dafa7841185b5
luigitarallo <tarallo.gigi@gmail.com>
2023-08-31 15:40:55 +0200

    fix  done property

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ createApp({
       toDo: [
         { text: "Pagare le bollette", done: true },
         { text: "Dar da mangiare al pesce sorro", done: false },
-        { text: "Fare la spesa", done: "true" },
+        { text: "Fare la spesa", done: true },
       ],
     };
   },
